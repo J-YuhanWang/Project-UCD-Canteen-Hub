846831b0808f7c05ac03aa0702ca24eb945fc1d0
J-YuhanWang <wangyuhan6459@gmail.com>
2026-01-22 21:30:32 +0000

    Refactor: Remove legacy code, keep infrastructure only for reconstruction.

---
+++ src/main/java/io/github/j_yuhanwang/food_ordering_app/enums/RoleType.java
@@ -0,0 +1,17 @@
+package io.github.j_yuhanwang.food_ordering_app.enums;/*
+ * @author BlairWang
+ * @Date 18/01/2026 6:35 pm
+ * @Version 1.0
+ */
+
+public enum RoleType {
+    //user-student-customer
+    ROLE_STUDENT,
+    //canteen manager
+    ROLE_MERCHANT,
+    //school administrator
+    ROLE_ADMIN,
+    //runner
+    ROLE_RUNNER
+
+}
