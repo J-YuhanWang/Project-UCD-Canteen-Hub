109d3a913ff27b9b63d82e39d261931921b2d8a6
J-YuhanWang <wangyuhan6459@gmail.com>
2026-02-02 16:51:49 +0000

    feat(dto): add Canteen and Menu related DTOs

---
+++ src/main/java/io/github/j_yuhanwang/food_ordering_app/canteen/dtos/CanteenDTO.java
@@ -0,0 +1,51 @@
+package io.github.j_yuhanwang.food_ordering_app.canteen.dtos;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import io.github.j_yuhanwang.food_ordering_app.canteen.entity.CanteenSchedule;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalTime;
+import java.util.List;
+
+/**
+ * @author YuhanWang
+ * @Date 02/02/2026 2:23 pm
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class CanteenDTO {
+
+    private Long id;
+
+    @NotBlank(message = "Canteen name is required")
+    private String name;
+
+    private String canteenType;
+
+    private String description;
+
+    private String imageUrl;
+
+    //calculated by service layer
+    private boolean isOpen;
+
+    @JsonFormat(pattern = "HH:mm")
+    private LocalTime currentOpeningTime;
+
+    @JsonFormat(pattern = "HH:mm")
+    private LocalTime currentClosingTime;
+
+    private List<CanteenScheduleDTO> schedules;
+
+    //List<MenuDTO> menus and User manager not shown in DTO
+}
---
+++ src/main/java/io/github/j_yuhanwang/food_ordering_app/menu/dtos/MenuDTO.java
@@ -0,0 +1,65 @@
+package io.github.j_yuhanwang.food_ordering_app.menu.dtos;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalTime;
+
+/**
+ * @author YuhanWang
+ * @Date 02/02/2026 4:28 pm
+ */
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class MenuDTO {
+    private Long id;
+
+    @NotBlank(message = "Dish name is required")
+    private String name;
+
+    private String description;
+
+    /**
+     * Use BigDecimal for money to avoid precision loss.
+     * @Positive ensures price is greater than 0.
+     */
+    @NotNull(message = "Price is required")
+    @Positive(message = "Price must be greater than 0")
+    private BigDecimal price;
+
+    private String imageUrl;
+
+    private String foodCategory; //"Drinks","Mains"
+
+    //Availability Logic
+    private boolean isAvailable;
+
+    @JsonFormat(pattern = "HH:mm")
+    private LocalTime availableStartTime;
+
+    @JsonFormat(pattern = "HH:mm")
+    private LocalTime availableEndTime;
+
+    //Key Relationships
+    // Reference ID for database relationships and routing.
+    @NotNull(message = "Canteen id is required")
+    private Long canteenId;
+
+    // Redundant name to avoid extra API calls for UI display.
+    private String canteenName;
+
+}
