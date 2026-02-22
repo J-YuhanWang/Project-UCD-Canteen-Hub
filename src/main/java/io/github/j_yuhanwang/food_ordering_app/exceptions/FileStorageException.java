749198ed667b82c5aa9c75d3b9eefc060d643773
J-YuhanWang <wangyuhan6459@gmail.com>
2026-02-22 17:31:46 +0000

    docs: refine Javadoc comments in FileStorageException

--- src/main/java/io/github/j_yuhanwang/food_ordering_app/exceptions/FileStorageException.java
+++ src/main/java/io/github/j_yuhanwang/food_ordering_app/exceptions/FileStorageException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
- * Custom exception to indicate that a requested resource was not found.
+ * Custom exception to indicate a failure during file storage operations (e.g., AWS S3 network outages).
  * Inherits from RuntimeException to avoid mandatory try-catch blocks (Unchecked Exception).
  * @author YuhanWang
  * @Date 15/02/2026 2:44 pm
