5cd466b3f4b427b00e3defc068a2d5d2cec4ad1a
J-YuhanWang <wangyuhan6459@gmail.com>
2026-03-18 20:35:48 +0000

    feat(user): implement full UserServiceImpl with AWS S3, Security, and Admin actions

--- src/main/java/io/github/j_yuhanwang/food_ordering_app/auth_users/services/UserServiceImpl.java
+++ src/main/java/io/github/j_yuhanwang/food_ordering_app/auth_users/services/UserServiceImpl.java
@@ -8,14 +8,15 @@ import io.github.j_yuhanwang.food_ordering_app.auth_users.repository.UserReposit
 import io.github.j_yuhanwang.food_ordering_app.aws.services.AwsS3Service;
 import io.github.j_yuhanwang.food_ordering_app.email_notification.dtos.NotificationDTO;
 import io.github.j_yuhanwang.food_ordering_app.email_notification.services.NotificationService;
-import io.github.j_yuhanwang.food_ordering_app.enums.NotificationType;
 import io.github.j_yuhanwang.food_ordering_app.enums.RoleType;
 import io.github.j_yuhanwang.food_ordering_app.enums.UserStatus;
+import io.github.j_yuhanwang.food_ordering_app.exceptions.BadRequestException;
 import io.github.j_yuhanwang.food_ordering_app.exceptions.ResourceNotFoundException;
-import io.github.j_yuhanwang.food_ordering_app.role.dtos.RoleDTO;
+import io.github.j_yuhanwang.food_ordering_app.role.entity.Role;
+import io.github.j_yuhanwang.food_ordering_app.role.repository.RoleRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -24,6 +25,7 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.UUID;
 
 /**
  * @author YuhanWang
@@ -39,6 +41,7 @@ public class UserServiceImpl implements UserService {
     private final UserMapper userMapper;
     private final NotificationService notificationService;
     private final AwsS3Service awsS3Service;
+    private final RoleRepository roleRepository;
 
     //Internal helper
     @Override
@@ -80,8 +83,10 @@ public class UserServiceImpl implements UserService {
     @Transactional
     public void deactivateOwnAccount() {
         log.info("Request to deactivate account for current logged-in user");
-
+        //fetch the user
         User user = getCurrentLoggedInUser();
+
+        //change the status to inactive
         user.setUserStatus(UserStatus.INACTIVE);
         userRepository.save(user);
 
@@ -98,27 +103,99 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional
     public UserDTO uploadAvatar(MultipartFile file) {
-        return null;
+        log.info("Request to upload new avatar for the current user");
+        //fetch the user
+        User user = getCurrentLoggedInUser();
+        //Delete the old image in cloud if it exists at first, but not throw the exception
+        if(StringUtils.hasText(user.getProfileUrl())){
+            try{
+                String oldUrl = user.getProfileUrl();
+                //substring(index + 1): Retrieves the filename after "/".
+                String oldKey = "profile/" + oldUrl.substring(oldUrl.lastIndexOf("/") + 1);
+                awsS3Service.deleteFile(oldKey);
+                log.info("Deleted old avatar from S3");
+            }catch(Exception e){
+                log.error("Failed to delete old avatar from S3, proceeding with upload: {}", e.getMessage());
+            }
+        }
+
+        //upload new image
+        String fileName = UUID.randomUUID().toString()+"_"+file.getOriginalFilename();
+        String keyName = "profile/"+fileName;//save inside profile folder
+        String newProfileUrl = awsS3Service.uploadFile(keyName,file);
+
+        //update the url data to repository
+        user.setProfileUrl(newProfileUrl);
+        User savedUser = userRepository.save(user);
+        log.info("New avatar uploaded and database updated: {}", newProfileUrl);
+
+        return userMapper.toDTO(savedUser);
     }
 
     @Override
+    @Transactional
     public void changePassword(ChangePasswordRequest request) {
+        //fetch the user
+        User user = getCurrentLoggedInUser();
+        //Exception 1: input old password not match the saved user pwd
+        if(!passwordEncoder.matches(request.getCurrentPassword(),user.getPassword())){
+            throw new BadRequestException("Current password does not match.");
+        }
+        //Exception 2: new password is empty
+        if(!StringUtils.hasText(request.getNewPassword())){
+            throw new BadRequestException("New password cannot be empty.");
+        }
+        //Exception 3: New password and confirmation do not match.
+        if(!request.getNewPassword().equals(request.getConfirmPassword())){
+            throw new BadRequestException("New password and confirmation do not match.");
+        }
 
+        //One-way Hashing
+        user.setPassword(passwordEncoder.encode(request.getNewPassword()));
+        userRepository.save(user);
+
+        log.info("Password successfully updated for user: {}", user.getEmail());
     }
 
     @Override
     public List<UserDTO> getAllUsers() {
-        return List.of();
+        List<User> userList =  userRepository.findAll(Sort.by(Sort.Direction.DESC,"id"));
+        return userList.stream()
+                //.map(user->userMapper.toDTO(user))
+                .map(userMapper::toDTO)
+                .toList();
     }
 
+    //For admin role: Update status (banned or reactivated user)
     @Override
+    @Transactional
     public void updateUserStatus(Long userId, UserStatus status) {
+        //fetch the user
+        User user = userRepository.findById(userId).orElseThrow(
+                ()->new ResourceNotFoundException("User","id",userId)
+        );
 
+        user.setUserStatus(status);
+        userRepository.save(user);
     }
 
+    //For admin role: Change role (upgrade to manager or downgrade)
     @Override
+    @Transactional
     public void updateUserRole(Long userId, RoleType role) {
+        //fetch the user
+        User user = userRepository.findById(userId).orElseThrow(
+                ()->new ResourceNotFoundException("User","id",userId)
+        );
+        //Find Role by Name
+        Role newRole = roleRepository.findByName(role.name()).orElseThrow(
+                ()->new ResourceNotFoundException("Role","name",role.name())
+        );
 
+        user.getRoles().clear();
+        user.getRoles().add(newRole);
+        userRepository.save(user);
     }
 }
